cd01c32ce8474d1f7dc510acef2048853c68a2bc
svetlyr <radu.svetly@protonmail.com>
2024-11-30 22:25:32 +0200

    feat(flash): enable enhanced search

--- lua/config-vscode/flash.lua
+++ lua/config-vscode/flash.lua
@@ -3,10 +3,14 @@ return {
   event = "VeryLazy",
 
   ---@type Flash.Config
-  opts = {},
+  opts = {
+    modes = {
+      search = {
+        enabled = true,
+      },
+    },
+  },
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    -- { "<C-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 }
